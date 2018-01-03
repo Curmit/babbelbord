0c5ba82c9cc033d97d46f87994b07d6034b0ba76
Carmen <Carmen@wlan176089.mobiel.utwente.nl>
2018-01-03 10:13:07 +0100

    add pinMode

---
+++ Verison1/Verison1.ino
@@ -0,0 +1,11 @@
+// This is version1: HEllO 
+void setup() {
+  pinMode(2, OUTPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
